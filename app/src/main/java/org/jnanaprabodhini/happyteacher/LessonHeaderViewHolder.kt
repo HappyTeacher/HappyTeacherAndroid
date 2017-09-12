e2c50fee8c56e2afb126c2d698fa3c6768df65c1
Graham Earley <graham.p.earley@gmail.com>
2017-09-12 13:00:33 +0530

    Specify view types explicitly for null safety

--- app/src/main/java/org/jnanaprabodhini/happyteacher/LessonHeaderViewHolder.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/LessonHeaderViewHolder.kt
@@ -2,15 +2,16 @@ package org.jnanaprabodhini.happyteacher
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.list_item_lesson.view.*
 
 /**
  * Created by grahamearley on 9/12/17.
  */
 class LessonHeaderViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-    val lessonTitleTextView = itemView.lessonTitleTextView
-    val authorNameTextView = itemView.authorNameTextView
-    val institutionTextView = itemView.institutionTextView
-    val locationTextView = itemView.locationTextView
-    val dateEditedTextView = itemView.dateEditedTextView
+    val lessonTitleTextView: TextView = itemView.lessonTitleTextView
+    val authorNameTextView: TextView = itemView.authorNameTextView
+    val institutionTextView: TextView = itemView.institutionTextView
+    val locationTextView: TextView = itemView.locationTextView
+    val dateEditedTextView: TextView = itemView.dateEditedTextView
 }
--- app/src/main/java/org/jnanaprabodhini/happyteacher/TopicViewHolder.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/TopicViewHolder.kt
@@ -2,9 +2,10 @@ package org.jnanaprabodhini.happyteacher
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.list_item_topic.view.*
 
 class TopicViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)  {
-    val topicTextView = itemView.topicTextView
-    val lessonsRecyclerView = itemView.lessonsRecyclerView
+    val topicTextView: TextView = itemView.topicTextView
+    val lessonsRecyclerView: RecyclerView = itemView.lessonsRecyclerView
 }
