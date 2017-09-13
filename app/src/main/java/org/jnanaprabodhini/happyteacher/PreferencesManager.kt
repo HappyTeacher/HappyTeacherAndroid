c7aebc59597287466a9299bc07ba36222920ae74
Graham Earley <graham.p.earley@gmail.com>
2017-09-13 14:15:36 +0530

    Make properties private on preferences

--- app/src/main/java/org/jnanaprabodhini/happyteacher/PreferencesManager.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/PreferencesManager.kt
@@ -7,17 +7,15 @@ import android.content.SharedPreferences
  * Created by grahamearley on 9/13/17.
  */
 class PreferencesManager(val context: Context) {
-    val PREFS_NAME = "DEFAULT_PREFS"
-    val preferences: SharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+    private val PREFS_NAME = "DEFAULT_PREFS"
+    private val preferences: SharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
 
-    val BOARD = "BOARD"
+    private val BOARD = "BOARD"
 
-    fun setBoard(boardKey: String) {
+    fun setBoardKey(boardKey: String) {
         preferences.edit().putString(BOARD, boardKey)
     }
 
-    fun getBoard() {
-        preferences.getString(BOARD, context.getString(R.string.maharashtra_state_board))
-    }
+    fun getBoardKey(): String = preferences.getString(BOARD, context.getString(R.string.maharashtra_state_board))
 
 }
