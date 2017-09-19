8c95369ab6e327aae59c6062bec13e254590498b
Graham Earley <graham.p.earley@gmail.com>
2017-09-19 17:07:06 +0530

    Make DataObserver interface functions all optional

--- app/src/main/java/org/jnanaprabodhini/happyteacher/DataObserver.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/DataObserver.kt
@@ -4,11 +4,11 @@ package org.jnanaprabodhini.happyteacher
  * Created by grahamearley on 9/18/17.
  */
 interface DataObserver {
-    fun onRequestNewData()
+    fun onRequestNewData() {}
 
-    fun onDataLoaded()
+    fun onDataLoaded() {}
 
-    fun onDataEmpty()
+    fun onDataEmpty() {}
 
-    fun onDataNonEmpty()
+    fun onDataNonEmpty() {}
 }
