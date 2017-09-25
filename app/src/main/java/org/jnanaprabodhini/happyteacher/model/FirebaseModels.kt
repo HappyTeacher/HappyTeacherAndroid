6e3d19ad435a912e36d32e028ff019116bb8150d
Graham Earley <graham.p.earley@gmail.com>
2017-09-25 11:44:58 +0530

    Add data class models for subtopic lessons and cards (and clean up old models)

--- app/src/main/java/org/jnanaprabodhini/happyteacher/model/FirebaseModels.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/model/FirebaseModels.kt
@@ -13,16 +13,9 @@ data class Subject(var name: String = "",
 data class Topic(var name: String = "",
                  var subject: String = "")
 
+data class Board(var name: String = "")
 
-data class SubtopicLessonHeader(var name: String = "",
-                                var authorId: String = "",
-                                var authorInstitution: String = "",
-                                var authorLocation: String = "",
-                                var authorName: String = "",
-                                var dateEdited: Long = 0)
-
-data class Subtopic(var name: String = "",
-                    var featured: SubtopicLessonHeader = SubtopicLessonHeader())
+data class Subtopic(var name: String = "")
 
 data class SyllabusLesson(var board: String = "",
                           var lessonNumber: Int = 0,
@@ -31,4 +24,51 @@ data class SyllabusLesson(var board: String = "",
                           var subject: String = "",
                           var topicCount: Int = 0)
 
-data class Board(var name: String = "")
+data class SubtopicLessonHeader(var name: String = "",
+                                var authorEmail: String = "",
+                                var authorInstitution: String = "",
+                                var authorLocation: String = "",
+                                var authorName: String = "",
+                                var dateEdited: Long = 0,
+                                var lesson: String = "",
+                                var subtopic: String = "")
+
+data class SubtopicLesson(var name: String = "",
+                          var authorEmail: String = "",
+                          var authorInstitution: String = "",
+                          var authorLocation: String = "",
+                          var authorName: String = "",
+                          var dateEdited: Long = 0,
+                          var cards: Map<Int, LessonCard> = emptyMap()) {
+    fun getCards(): Collection<LessonCard> = cards.values
+}
+
+data class LessonCard(var header: String = "",
+                      var body: String = "",
+                      var imageUrls: Map<Int, String> = emptyMap(),
+                      var videoUrl: String = "",
+                      var linkUrls: Map<Int, String> = emptyMap(),
+                      var attachmentUrl: String = "",
+                      var type: String = "",
+                      var number: Int = 0) {
+
+    fun getLinkUrls(): Collection<String> = linkUrls.values
+    fun getImageUrls(): Collection<String> = imageUrls.values
+    fun getCardType(): CardType = CardType.getTypeFromString(type)
+
+    enum class CardType {
+        VIDEO, IMAGE, PLAIN, UNSUPPORTED;
+
+        companion object {
+            fun getTypeFromString(typeString: String): CardType {
+                when(typeString) {
+                    "video" -> return VIDEO
+                    "image" -> return IMAGE
+                    "plain" -> return PLAIN
+                    else -> return UNSUPPORTED
+                }
+            }
+        }
+
+    }
+}
