78880ae6ca1cd532577b8ab133a405c32e5b0413
Graham Earley <graham.p.earley@gmail.com>
2017-10-09 12:23:19 +0530

    Add submission count to card header view

--- app/src/main/java/org/jnanaprabodhini/happyteacher/adapter/viewholder/SubtopicHeaderViewHolder.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/adapter/viewholder/SubtopicHeaderViewHolder.kt
@@ -3,7 +3,7 @@ package org.jnanaprabodhini.happyteacher.adapter.viewholder
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
-import kotlinx.android.synthetic.main.list_item_lesson.view.*
+import kotlinx.android.synthetic.main.list_item_lesson_header.view.*
 
 /**
  * Created by grahamearley on 9/12/17.
@@ -14,4 +14,5 @@ class SubtopicHeaderViewHolder(itemView: View): RecyclerView.ViewHolder(itemView
     val institutionTextView: TextView = itemView.institutionTextView
     val locationTextView: TextView = itemView.locationTextView
     val dateEditedTextView: TextView = itemView.dateEditedTextView
+    val submissionCountTextView: TextView = itemView.submissionCountTextView
 }
