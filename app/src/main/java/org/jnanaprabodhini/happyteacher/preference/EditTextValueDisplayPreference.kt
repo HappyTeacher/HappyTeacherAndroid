64a3e18b22e1ce8f149ce3f10bde2a57e9355e9c
Graham Earley <graham.p.earley@gmail.com>
2017-10-30 13:20:59 +0530

    Only use one custom pref class — with attr for showing dialog or not

--- app/src/main/java/org/jnanaprabodhini/happyteacher/preference/EditTextValueDisplayPreference.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/preference/EditTextValueDisplayPreference.kt
@@ -3,11 +3,28 @@ package org.jnanaprabodhini.happyteacher.preference
 import android.content.Context
 import android.support.v7.preference.EditTextPreference
 import android.util.AttributeSet
+import android.content.res.TypedArray
+import org.jnanaprabodhini.happyteacher.R
+
 
 /**
  * Created by grahamearley on 10/30/17.
  */
 open class EditTextValueDisplayPreference(context: Context, attrs: AttributeSet): EditTextPreference(context, attrs) {
+
+    private var showDialogOnClick = true
+
+    init {
+        val attributes = context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.EditTextValueDisplayPreference,
+                0, 0)
+        try {
+            showDialogOnClick = attributes.getBoolean(R.styleable.EditTextValueDisplayPreference_showDialogOnClick, true)
+        } finally {
+            attributes.recycle()
+        }
+    }
 
     override fun persistString(value: String?): Boolean {
         val persisted = super.persistString(value)
@@ -21,5 +38,9 @@ open class EditTextValueDisplayPreference(context: Context, attrs: AttributeSet)
         } else {
             text
         }
+    }
+
+    override fun onClick() {
+        if (showDialogOnClick) super.onClick()
     }
 }
