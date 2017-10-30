bacf2404658fdffb94df0bd9442ad56ee1ad340b
Graham Earley <graham.p.earley@gmail.com>
2017-10-30 11:48:40 +0530

    Fill in listener for prefs changes

--- app/src/main/java/org/jnanaprabodhini/happyteacher/activity/SettingsActivity.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/activity/SettingsActivity.kt
@@ -3,43 +3,64 @@ package org.jnanaprabodhini.happyteacher.activity
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceFragment
+import com.google.firebase.auth.UserProfileChangeRequest
 import org.jnanaprabodhini.happyteacher.R
 import org.jnanaprabodhini.happyteacher.activity.base.HappyTeacherActivity
-import org.jnanaprabodhini.happyteacher.activity.SettingsActivity.SettingsFragment
 
 
+class SettingsActivity : HappyTeacherActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
 
-class SettingsActivity : HappyTeacherActivity() {
+    lateinit var settingsFragment: SettingsFragment
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        fragmentManager.beginTransaction()
-                .replace(android.R.id.content, SettingsFragment())
-                .commit()
-    }
-
-    class SettingsFragment: PreferenceFragment(), SharedPreferences.OnSharedPreferenceChangeListener {
+    class SettingsFragment: PreferenceFragment() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             addPreferencesFromResource(R.xml.preferences)
         }
+    }
 
-        override fun onResume() {
-            super.onResume()
-            preferenceScreen.sharedPreferences
-                    .registerOnSharedPreferenceChangeListener(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        settingsFragment = SettingsFragment()
+        fragmentManager.beginTransaction()
+                .replace(android.R.id.content, settingsFragment)
+                .commit()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        settingsFragment.preferenceScreen.sharedPreferences
+                .registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        settingsFragment.preferenceScreen.sharedPreferences
+                .unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(preferences: SharedPreferences?, key: String?) {
+        when(key) {
+            getString(R.string.prefs_key_user_name) -> onNameChange(preferences?.getString(key, ""))
+            getString(R.string.prefs_key_user_institution) -> onInstitutionChange(preferences?.getString(key, ""))
+            getString(R.string.prefs_key_user_location) -> onLocationChange(preferences?.getString(key, ""))
         }
+    }
 
-        override fun onPause() {
-            super.onPause()
-            preferenceScreen.sharedPreferences
-                    .unregisterOnSharedPreferenceChangeListener(this)
-        }
+    private fun onNameChange(newName: String?) {
+        val nameChangeRequest = UserProfileChangeRequest.Builder().setDisplayName(newName).build()
+        auth.currentUser?.updateProfile(nameChangeRequest)
 
-        override fun onSharedPreferenceChanged(preferences: SharedPreferences?, key: String?) {
+        // TODO: save to firestore
+    }
 
-        }
+    private fun onInstitutionChange(newInstitution: String?) {
+        // todo: save to firestore
+    }
+
+    private fun onLocationChange(newLocation: String?) {
+        // todo: save to Firestore
     }
 
 }
