fcacee63608978e233da9557136786f221998a0c
Graham Earley <graham.p.earley@gmail.com>
2017-11-02 15:53:27 +0530

    Add layout manager (whoops) and observer callbacks to subtopic choice
    and appbarlayout scrolling fix

--- app/src/main/java/org/jnanaprabodhini/happyteacher/activity/SubtopicChoiceActivity.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/activity/SubtopicChoiceActivity.kt
@@ -1,7 +1,10 @@
 package org.jnanaprabodhini.happyteacher.activity
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_subtopic_choice.*
 import kotlinx.android.synthetic.main.stacked_subject_spinners.*
@@ -9,18 +12,20 @@ import org.jnanaprabodhini.happyteacher.R
 import org.jnanaprabodhini.happyteacher.activity.base.HappyTeacherActivity
 import org.jnanaprabodhini.happyteacher.adapter.firestore.TopicsRecyclerAdapter
 import org.jnanaprabodhini.happyteacher.adapter.helper.FirebaseDataObserver
+import org.jnanaprabodhini.happyteacher.extension.setVisibilityGone
+import org.jnanaprabodhini.happyteacher.extension.setVisible
 import org.jnanaprabodhini.happyteacher.model.CardListContentHeader
 import org.jnanaprabodhini.happyteacher.model.Topic
 import org.jnanaprabodhini.happyteacher.view.SubjectSpinnerManager
 
 class SubtopicChoiceActivity : HappyTeacherActivity(), FirebaseDataObserver {
-    // TODO: Data observer!
 
     private val subjectSpinnerManager = SubjectSpinnerManager(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_subtopic_choice)
+        topicsRecyclerView.layoutManager = LinearLayoutManager(this)
 
         initializeSpinners()
     }
@@ -52,5 +57,34 @@ class SubtopicChoiceActivity : HappyTeacherActivity(), FirebaseDataObserver {
 
         topicAdapter.startListening()
         topicsRecyclerView.adapter = topicAdapter
+    }
+
+    override fun onRequestNewData() {
+        progressBar.setVisible()
+        topicsRecyclerView.setVisibilityGone()
+        statusTextView.setVisibilityGone()
+    }
+
+    override fun onDataLoaded() {
+        progressBar.setVisibilityGone()
+    }
+
+    override fun onDataEmpty() {
+        topicsRecyclerView.setVisibilityGone()
+        statusTextView.setVisible()
+        statusTextView.setText(R.string.there_are_no_topics_for_this_subject_yet)
+    }
+
+    override fun onDataNonEmpty() {
+        statusTextView.setVisibilityGone()
+        topicsRecyclerView.setVisible()
+    }
+
+    override fun onError(e: FirebaseFirestoreException?) {
+        progressBar.setVisibilityGone()
+        topicsRecyclerView.setVisibilityGone()
+
+        statusTextView.setVisible()
+        statusTextView.setText(R.string.there_was_an_error_loading_topics_for_this_subject)
     }
 }
