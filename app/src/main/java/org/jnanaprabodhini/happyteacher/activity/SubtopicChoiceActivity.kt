b81c1bdf1c44523ad39f0b4fb4294635796cd3a5
Graham Earley <graham.p.earley@gmail.com>
2017-11-02 13:18:51 +0530

    Add subtopic choice activity

---
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/activity/SubtopicChoiceActivity.kt
@@ -0,0 +1,14 @@
+package org.jnanaprabodhini.happyteacher.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.jnanaprabodhini.happyteacher.R
+import org.jnanaprabodhini.happyteacher.activity.base.HappyTeacherActivity
+
+class SubtopicChoiceActivity : HappyTeacherActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subtopic_choice)
+    }
+}
