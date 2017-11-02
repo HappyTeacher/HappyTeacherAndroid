1b57854ebff7c318b09596ad3b5b0fb79e31874d
Graham Earley <graham.p.earley@gmail.com>
2017-11-02 13:16:45 +0530

    Extract subject spinner + display logic from TopicsListActivity into manager class

--- app/src/main/java/org/jnanaprabodhini/happyteacher/activity/TopicsListActivity.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/activity/TopicsListActivity.kt
@@ -3,12 +3,7 @@ package org.jnanaprabodhini.happyteacher.activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.annotation.IntegerRes
-import android.support.annotation.LayoutRes
 import android.support.v7.widget.LinearLayoutManager
-import android.view.View
-import android.widget.Spinner
-import android.widget.TextView
-import com.crashlytics.android.Crashlytics
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.Query
@@ -16,13 +11,12 @@ import kotlinx.android.synthetic.main.activity_topics_list.*
 import kotlinx.android.synthetic.main.header_syllabus_lesson_topic.*
 import org.jnanaprabodhini.happyteacher.R
 import org.jnanaprabodhini.happyteacher.activity.base.BottomNavigationActivity
-import org.jnanaprabodhini.happyteacher.adapter.firestore.FirestoreObserverListAdapter
 import org.jnanaprabodhini.happyteacher.adapter.firestore.TopicsRecyclerAdapter
 import org.jnanaprabodhini.happyteacher.adapter.helper.FirebaseDataObserver
 import org.jnanaprabodhini.happyteacher.extension.*
 import org.jnanaprabodhini.happyteacher.model.CardListContentHeader
-import org.jnanaprabodhini.happyteacher.model.Subject
 import org.jnanaprabodhini.happyteacher.model.Topic
+import org.jnanaprabodhini.happyteacher.view.SubjectSpinnerTopicListManager
 
 class TopicsListActivity : BottomNavigationActivity(), FirebaseDataObserver {
 
@@ -113,8 +107,16 @@ class TopicsListActivity : BottomNavigationActivity(), FirebaseDataObserver {
         hideSyllabusLessonTopicHeader()
         topicsProgressBar.setVisible()
 
-        // Show all topics for a subject, selected by spinner
-        setupParentSubjectSpinner()
+        // Create a SubjectSpinnerTopicListManager to control the subject spinners
+        //  and recycler view.
+        val spinnerListManager = SubjectSpinnerTopicListManager(parentSubjectSpinner, childSubjectSpinner,
+                topicsProgressBar, this,
+                {subjectKey -> updateListOfTopicsForSubject(subjectKey)})
+
+        spinnerListManager.parentSpinnerSelectionIndex = parentSubjectSelectionIndex
+        spinnerListManager.childSpinnerSelectionIndex = childSubjectSelectionIndex
+
+        spinnerListManager.initializeSpinners()
     }
 
     override fun onBottomNavigationItemReselected() {
@@ -124,54 +126,6 @@ class TopicsListActivity : BottomNavigationActivity(), FirebaseDataObserver {
         } else {
             // Scroll to top of topic list:
             topicsRecyclerView.smoothScrollToPosition(0)
-        }
-    }
-
-    private fun setupParentSubjectSpinner() {
-        // Parent subjects have no parents, so pass null as value for parentSubject:
-        setupSpinner(parentSubjectSpinner, R.layout.spinner_item, null, parentSubjectSelectionIndex)
-    }
-
-    /**
-     *  Set up one of the two spinners (the subject spinner or the sub-subject spinner).
-     *
-     *  The subject spinner is shown when this activity is not being used
-     *   to display topics relevant to a specific syllabus lesson plan.
-     */
-    private fun setupSpinner(spinner: Spinner, @LayoutRes spinnerLayout: Int, parentSubjectId: String?, selectionIndex: Int) {
-        val subjectQuery = firestoreLocalized.collection(getString(R.string.subjects)).whereEqualTo(getString(R.string.parent_subject), parentSubjectId)
-
-        val spinnerDataObserver = object: FirebaseDataObserver {
-            override fun onDataNonEmpty() {
-                topicsProgressBar.setVisibilityGone()
-                spinner.setVisible()
-            }
-            // todo: add on empty, error, etc.
-        }
-
-        val adapter = object: FirestoreObserverListAdapter<Subject>(subjectQuery, Subject::class.java, spinnerLayout, spinnerDataObserver, this) {
-            override fun populateView(view: View, model: Subject, position: Int) {
-                (view as TextView).text = model.name
-            }
-        }
-
-        adapter.startListening()
-        spinner.adapter = adapter
-
-        spinner.selectIndexWhenPopulated(selectionIndex)
-
-        spinner.onItemSelected { position ->
-            val subject = adapter.getItem(position)
-            val selectedSubjectKey = adapter.getItemKey(position)
-
-            if (subject.hasChildren) {
-                setupSpinner(childSubjectSpinner, R.layout.spinner_item_child, selectedSubjectKey, childSubjectSelectionIndex)
-            } else if (!subject.hasChildren && spinner == childSubjectSpinner) {
-                updateListOfTopicsForSubject(selectedSubjectKey)
-            } else {
-                updateListOfTopicsForSubject(selectedSubjectKey)
-                childSubjectSpinner.setVisibilityGone()
-            }
         }
     }
 
