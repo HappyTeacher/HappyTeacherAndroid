fae5b9791eca059cacde14a02e76a02ef80d7776
Graham Earley <graham.p.earley@gmail.com>
2017-11-03 14:28:18 +0530

    Add ‘+’ fab for lesson editor

--- app/src/main/java/org/jnanaprabodhini/happyteacher/activity/LessonEditorActivity.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/activity/LessonEditorActivity.kt
@@ -5,9 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.firestore.CollectionReference
+import kotlinx.android.synthetic.main.activity_card_list_content_viewer.*
 import org.jnanaprabodhini.happyteacher.R
 import org.jnanaprabodhini.happyteacher.adapter.contentlist.CardListContentRecyclerAdapter
 import org.jnanaprabodhini.happyteacher.adapter.contentlist.EditableLessonRecyclerAdapter
+import org.jnanaprabodhini.happyteacher.extension.setVisible
 import org.jnanaprabodhini.happyteacher.model.CardListContentHeader
 import org.jnanaprabodhini.happyteacher.model.ContentCard
 import java.io.File
@@ -33,6 +35,12 @@ class LessonEditorActivity: CardListContentViewerActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.setSubtitle(R.string.lesson_editor)
+        setupFab()
+    }
+
+    private fun setupFab() {
+        fab.setVisible()
+        // todo: onclick -> Card Editor for new card
     }
 
     override fun getCardRecyclerAdapter(cardRef: CollectionReference, attachmentDestinationDirectory: File): CardListContentRecyclerAdapter {
