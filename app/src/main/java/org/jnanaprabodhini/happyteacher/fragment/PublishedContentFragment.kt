167012ea6a1fbc2f0be7216459b71d718756d0fe
Graham Earley <graham.p.earley@gmail.com>
2017-11-15 15:38:36 +0530

    Add functional placeholders for submission/published fragments

--- app/src/main/java/org/jnanaprabodhini/happyteacher/fragment/PublishedContentFragment.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacher/fragment/PublishedContentFragment.kt
@@ -1,14 +1,31 @@
 package org.jnanaprabodhini.happyteacher.fragment
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import kotlinx.android.synthetic.main.fragment_recycler.*
 import org.jnanaprabodhini.happyteacher.R
+import org.jnanaprabodhini.happyteacher.adapter.firestore.DraftHeaderRecyclerAdapter
+import org.jnanaprabodhini.happyteacher.model.CardListContentHeader
 
 class PublishedContentFragment : RecyclerFragment() {
 
-    override val emptyRecyclerText: String by lazy { getString(R.string.you_have_no_submitted_contributions) }
-    override val errorText: String by lazy { getString(R.string.there_was_an_error_loading_your_submissions) }
+    override val emptyRecyclerText: String by lazy { getString(R.string.you_have_no_published_contributions) }
+    override val errorText: String by lazy { getString(R.string.there_was_an_error_loading_your_published_contributions) }
 
     override fun setupAdapter() {
-        // todo
+        val userId = auth.currentUser?.uid ?: return
+
+        val publishedQuery = firestoreLocalized.collection(getString(R.string.resources))
+                .whereEqualTo(getString(R.string.author_id), userId)
+                .whereEqualTo(getString(R.string.status), getString(R.string.status_awaiting_review))
+
+        val adapterOptions = FirestoreRecyclerOptions.Builder<CardListContentHeader>()
+                .setQuery(publishedQuery, CardListContentHeader::class.java).build()
+
+        // TODO: update adapter!
+        val adapter = DraftHeaderRecyclerAdapter(adapterOptions, this, activity)
+        adapter.startListening()
+
+        recyclerView.adapter = adapter
     }
 
 }
