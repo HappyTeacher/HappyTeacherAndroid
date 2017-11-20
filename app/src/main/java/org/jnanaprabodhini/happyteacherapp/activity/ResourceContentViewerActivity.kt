2ab6d04c357e702d9510e8f92062458bf26f57df
Graham Earley <graham.p.earley@gmail.com>
2017-11-20 13:43:54 +0530

    Add admin button for unpublishing resources

--- app/src/main/java/org/jnanaprabodhini/happyteacherapp/activity/ResourceContentViewerActivity.kt
+++ app/src/main/java/org/jnanaprabodhini/happyteacherapp/activity/ResourceContentViewerActivity.kt
@@ -5,11 +5,9 @@ import android.view.Menu
 import android.view.MenuItem
 import org.jnanaprabodhini.happyteacherapp.R
 import org.jnanaprabodhini.happyteacherapp.extension.showToast
-import org.jnanaprabodhini.happyteacherapp.model.User
 import org.jnanaprabodhini.happyteacherapp.util.FirestoreKeys
 import org.jnanaprabodhini.happyteacherapp.util.ResourceStatus
 import org.jnanaprabodhini.happyteacherapp.util.ResourceType
-import org.jnanaprabodhini.happyteacherapp.util.UserRoles
 
 /**
  * A parent activity for resource content viewers (as opposed to editors).
@@ -20,9 +18,12 @@ abstract class ResourceContentViewerActivity: ResourceContentActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_cards_viewer, menu)
         val editLessonMenuItem = menu?.findItem(R.id.menu_admin_edit_card_list_content)
-        val promoteToFeaturedLessonMenuItem = menu?.findItem(R.id.menu_admin_promote_to_featured)
+        val promoteToFeaturedLessonMenuItem = menu?.findItem(R.id.menu_mod_promote_to_featured)
+        val unpublishMenuItem = menu?.findItem(R.id.menu_admin_unpublish)
+
         editLessonMenuItem?.isVisible = false
         promoteToFeaturedLessonMenuItem?.isVisible = false
+        unpublishMenuItem?.isVisible = false
 
         // Only show admin/mod buttons to admins/mods!
         //  (Our Firestore security rules also only allow writes from admins)
@@ -37,6 +38,10 @@ abstract class ResourceContentViewerActivity: ResourceContentActivity() {
             promoteToFeaturedLessonMenuItem?.isVisible = true
         }
 
+        if (header.status == ResourceStatus.PUBLISHED && prefs.userIsAdmin()) {
+            unpublishMenuItem?.isVisible = true
+        }
+
         return true
     }
 
@@ -44,13 +49,22 @@ abstract class ResourceContentViewerActivity: ResourceContentActivity() {
         super.onOptionsItemSelected(item)
         when (item.itemId) {
             R.id.menu_admin_edit_card_list_content -> openInEditor()
-            R.id.menu_admin_promote_to_featured -> showPromoteToFeaturedLessonDialog()
+            R.id.menu_admin_unpublish -> unpublish()
+            R.id.menu_mod_promote_to_featured -> showPromoteToFeaturedLessonDialog()
         }
         return true
     }
 
     private fun openInEditor() {
         ResourceContentEditorActivity.launch(this, contentRef, header)
+    }
+
+    private fun unpublish() {
+        showToast(R.string.unpublishing)
+        contentRef.update(FirestoreKeys.STATUS, ResourceStatus.DRAFT)
+                .addOnSuccessListener {
+                    showToast(R.string.unpublished)
+                }
     }
 
     private fun showPromoteToFeaturedLessonDialog() {
